dfee60bf287e10663a6983aecc7b16c7a110a225
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2026-09-12 17:51:28 +0200

    core.global.LOG updated to match spdlog impl

--- data/core/global_scripts/std.lua
+++ data/core/global_scripts/std.lua
@@ -287,11 +287,13 @@ end
 
 ---@class core.global.LOG
 LOG = {
+	TRACE = 0,
     DEBUG = 1,
     INFO = 2,
     WARN = 3,
     ERROR = 4,
     FATAL = 5,
+	OFF = 6,
 }
 
 function lstg.MsgBoxWarn(msg)

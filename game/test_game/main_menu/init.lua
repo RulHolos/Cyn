18b09b7f156d78e5daa13f6be4732bde11cd2578
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2026-09-20 14:53:58 +0200

    a

--- game/test_game/main_menu/init.lua
+++ game/test_game/main_menu/init.lua
@@ -1,6 +1,5 @@
 local temple = require("yeva.backgrounds.temple")
 local stage_manager = require("cyn.gameplay.scene_manager")
-local player = require("yeva.player")
 local reimu = require("yeva.players.reimu")
 
 local entry = stage_manager:new_stage("loading", { entry_point = true })
